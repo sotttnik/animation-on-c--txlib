2849dbff4922dd46090d256a274723fc45964a60
sotttnik <122227836+sotttnik@users.noreply.github.com>
2023-01-20 14:19:32 +0400

    Add files via upload
    v2: Added animation scripts

--- WORK.cpp
+++ WORK.cpp
@@ -1,9 +1,10 @@
 #include "TXLib.h"
 
 void fox_draw (double x, double y, double size, double dHandL, double dHandR ,double dEarL, double dEarR, double dNose, int mirror);
-void bird_draw (double x, double y, double size, double dLegL, double dHandR);
+void bird_draw (double x, double y, double size, double dLegL, double dHandR, int mirror);
 void tree (double x, double y, double size);
 void tree_draw();
+void cheese (double x, double y, double size);
 
 int main()
     {
@@ -16,7 +17,23 @@ int main()
     tree_draw();
 
     //анимация
-/*   while (x < 1900)
+/*     while (y < 750)
+        {
+        x = 1100;
+        y = y + 30;
+        txSetFillColor(TX_WHITE);
+        txClear();
+
+        tree(x, y, 1);
+
+        txSleep(150);
+
+        }
+
+
+
+
+   while (x < 950)
         {
         if (z % -5 == -4)   {x = x + 40;}
         else              {x = x + 15; }
@@ -24,16 +41,56 @@ int main()
         txSetFillColor(TX_WHITE);
         txClear();
 
-        fox_draw (x, y - (-z)%5*25, 1, 0, 0, 0, 0, 0, 1);
+        fox_draw (x, y - (-z)%5*40, 1, 0, 0, 0, 0, 0, 1);
 
         txSleep(150);
 
         }
+
 */
+while (y < 775)
+    {
+    x = 300;
+    y = y + 10;
+    txSetFillColor(TX_WHITE);
+    txClear();
+    bird_draw (x, y, 0.5, 0, 0, -1);
+    txSleep(150);
+    }
+
+/*
+    while ( y < 775)
+     {
+        x = 300;
+        y = y + 25;
+        txSetFillColor(TX_WHITE);
+        txClear();
+        cheese(x,y,1);
+        bird_draw(x,y,0.5,0,0);
+        txSleep(150);
+
+
+     }
+*/
+   while (x < 1100)
+        {
+        if (z % 4 == 4)   {x = x + 40;}
+        else              {x = x + 15; }
+        z++;
+        y = y - 10;
+        txSetFillColor(TX_WHITE);
+        txClear();
+
+        bird_draw (x, y, 0.5, 0, 0, -1);
+        cheese(x,y,1);
+        txSleep(150);
+
+        }
+       tree_draw();
     }
 
 //-----------------------------------------------------------------------------
-void bird_draw (double x, double y, double size, double dLegL, double dLegR)
+void bird_draw (double x, double y, double size, double dLegL, double dLegR, int mirror)
 {
     txSetFillColor(TX_TRANSPARENT);
     txSetColor(TX_BLACK, 7*size);
@@ -41,38 +98,38 @@ void bird_draw (double x, double y, double size, double dLegL, double dLegR)
     //Body
     txSetFillColor(RGB(48, 48, 46));
     POINT BodyBird[6] = {{ROUND(x),            ROUND(y)},
-                         {ROUND(x + 150*size), ROUND(y -  50*size)},
+                         {ROUND(x + 150*size*mirror), ROUND(y -  50*size)},
                          {ROUND(x),            ROUND(y + 100*size)},
-                         {ROUND(x - 100*size), ROUND(y + 100*size)},
-                         {ROUND(x - 200*size), ROUND(y -  50*size)},
-                         {ROUND(x-  100*size), ROUND(y - 100*size)}};
+                         {ROUND(x - 100*size*mirror), ROUND(y + 100*size)},
+                         {ROUND(x - 200*size*mirror), ROUND(y -  50*size)},
+                         {ROUND(x-  100*size*mirror), ROUND(y - 100*size)}};
 
     txPolygon(BodyBird, 6);
 
     //Legs
-    txLine(x -  50*size, y + 100*size, x - 150*size,           y + 200*size);
-    txLine(x - 150*size, y + 200*size, x - 150*size + dLegL,   y + 225*size);
-    txLine(x - 150*size, y + 200*size, x - 125*size + dLegL,   y + 225*size);
-    txLine(x - 150*size, y + 200*size, x - 175*size + dLegL,   y + 225*size);
-    txLine(x -  25*size, y + 100*size, x -  50*size,           y + 200*size);
-    txLine(x -  50*size, y + 200*size, x -  50*size + dLegR,   y + 225*size);
-    txLine(x -  50*size, y + 200*size, x -  25*size + dLegR,   y + 225*size);
-    txLine(x -  50*size, y + 200*size, x -  75*size + dLegR,   y + 225*size);
+    txLine(x -  50*size*mirror, y + 100*size, x - 150*size        *mirror,   y + 200*size);
+    txLine(x - 150*size*mirror, y + 200*size, x - 150*size*mirror + dLegL,   y + 225*size);
+    txLine(x - 150*size*mirror, y + 200*size, x - 125*size*mirror + dLegL,   y + 225*size);
+    txLine(x - 150*size*mirror, y + 200*size, x - 175*size*mirror + dLegL,   y + 225*size);
+    txLine(x -  25*size*mirror, y + 100*size, x -  50*size*mirror,           y + 200*size);
+    txLine(x -  50*size*mirror, y + 200*size, x -  50*size*mirror + dLegR,   y + 225*size);
+    txLine(x -  50*size*mirror, y + 200*size, x -  25*size*mirror + dLegR,   y + 225*size);
+    txLine(x -  50*size*mirror, y + 200*size, x -  75*size*mirror + dLegR,   y + 225*size);
 
     //Head
     txSetFillColor(RGB(89, 88, 83));
-    txCircle(x - 175*size, y - 125*size, 75*size);
+    txCircle(x - 175*size*mirror, y - 125*size, 75*size);
 
     //Wings
     txSetFillColor(RGB(89, 88, 83));
     POINT WingFirst[3] = {{ROUND(x),            ROUND(y)},
-                          {ROUND(x - 100*size), ROUND(y +  50*size)},
-                          {ROUND(x - 100*size), ROUND(y - 100*size)}};
+                          {ROUND(x - 100*size*mirror), ROUND(y +  50*size)},
+                          {ROUND(x - 100*size*mirror), ROUND(y - 100*size)}};
 
 
     POINT WingSecond[3] = {{ROUND(x),            ROUND(y)},
-                           {ROUND(x - 100*size), ROUND(y + 150*size)},
-                           {ROUND(x - 100*size), ROUND(y - 100*size)}};
+                           {ROUND(x - 100*size*mirror), ROUND(y + 150*size)},
+                           {ROUND(x - 100*size*mirror), ROUND(y - 100*size)}};
     txPolygon(WingFirst, 3);
 
     txSetFillColor(RGB(56, 55, 53));
@@ -80,17 +137,17 @@ void bird_draw (double x, double y, double size, double dLegL, double dLegR)
 
     //Beak
     txSetFillColor(RGB(89, 88, 83));
-    POINT Beak[5] = {{ROUND(x - 200*size), ROUND(y - 150*size)},
-                     {ROUND(x - 300*size), ROUND(y - 150*size)},
-                     {ROUND(x - 200*size), ROUND(y - 100*size)},
-                     {ROUND(x - 300*size), ROUND(y -  50*size)},
-                     {ROUND(x - 200*size), ROUND(y -  75*size)}};
+    POINT Beak[5] = {{ROUND(x - 200*size*mirror), ROUND(y - 150*size)},
+                     {ROUND(x - 300*size*mirror), ROUND(y - 150*size)},
+                     {ROUND(x - 200*size*mirror), ROUND(y - 100*size)},
+                     {ROUND(x - 300*size*mirror), ROUND(y -  50*size)},
+                     {ROUND(x - 200*size*mirror), ROUND(y -  75*size)}};
     txPolygon(Beak, 5);
 
     //Eye
     txSetColor(TX_RED, 7*size);
     txSetFillColor(TX_RED);
-    txCircle(x - 150*size, y - 150*size, 5*size);
+    txCircle(x - 150*size*mirror, y - 150*size, 5*size);
 
 }
 
@@ -168,10 +225,30 @@ void tree (double x, double y, double size)
 
 void tree_draw ()
     {
+    txSetColor(TX_BLACK, 7);
     tree (1100, 200, 1);
     tree (1100, 350, 1);
     tree (1100, 500, 1);
+    txSetFillColor(TX_BROWN);
+    txRectangle(1075, 650, 1125,  750);
 
 
 
     }
+
+void cheese (double x, double y, double size)
+        {
+        POINT Cheese[3] =    {{ROUND (x      *size),ROUND      (y      *size)},
+                              {ROUND (x + 35 *size),ROUND      (y + 30*size) },
+                              {ROUND (x - 65 *size),ROUND      (y + 30*size)}};
+        txSetFillColor(RGB(255, 235, 54));
+        txPolygon(Cheese, 3);
+
+
+
+
+
+
+
+        }
+
