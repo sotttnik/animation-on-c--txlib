158b7cb534a4a52fe7ad6e07a724300ba47746bc
sotttnik <122227836+sotttnik@users.noreply.github.com>
2023-01-20 17:10:15 +0400

    Add files via upload
    v3: ENDED

--- WORK.cpp
+++ WORK.cpp
@@ -1,7 +1,7 @@
 #include "TXLib.h"
 
 void fox_draw (double x, double y, double size, double dHandL, double dHandR ,double dEarL, double dEarR, double dNose, int mirror);
-void bird_draw (double x, double y, double size, double dLegL, double dHandR, int mirror);
+void bird_draw (double x, double y, double size, double dLegL, double dLegR, int mirror);
 void tree (double x, double y, double size);
 void tree_draw();
 void cheese (double x, double y, double size);
@@ -29,36 +29,118 @@ int main()
         txSleep(150);
 
         }
-
-
-
-
-   while (x < 950)
-        {
-        if (z % -5 == -4)   {x = x + 40;}
-        else              {x = x + 15; }
-        z++;
+*/
+    while ( y < 700)
+     {
+        x = 500;
+        y = y + 25;
         txSetFillColor(TX_WHITE);
         txClear();
+        cheese(x,y,1);
+        bird_draw(300,775,0.5,0,0,-1);
+        txSleep(150);
 
-        fox_draw (x, y - (-z)%5*40, 1, 0, 0, 0, 0, 0, 1);
+
+     }
+   double size = 0.5;
+   while (x < 1100)
+        {
+
+        if (z % 4 == 4)   {x = x + 40;}
+        else              {x = x + 15; }
+        z++;
+        y = y - 10;
+        size = size - 0.002;
+
+
+        txSetFillColor(TX_WHITE);
+        txClear();
+        tree_draw();
+        bird_draw (x, y, size, 0, 0, -1);
+        cheese(x + 150,y - 70, 1);
+        txSleep(150);
+        }
+    x = 100;
+    y = 100;
+
+   double FSize = 0.1;
+
+   while (x < 650)
+        {
+        if (z % -5 == -4) {x = x + 40;}
+        else              {x = x + 15; }
+        z++;
+        y = y + 15;
+        FSize = FSize + 0.02;
+        txSetFillColor(TX_WHITE);
+        txClear();
+        tree_draw();
+        bird_draw(1125, 225, 0.4, 0,0,-1);
+        cheese(1275, 160,1);
+
+        fox_draw (x, y, FSize, z%2 * 10, 0, 0, 0, 0, 1);
 
         txSleep(150);
 
         }
+    x = 650;
+    while ( x < 1250)
+        {
+        x = x + 15;
+        txSetFillColor(TX_WHITE);
+        txClear();
+        tree_draw();
+        bird_draw(1125, 225, 0.4, 0,0,-1);
+        cheese(1275, 160,1);
+        fox_draw(x, 650, 0.8, 0, 0, 0, 0, 0, 1);
+        txSleep(150);
+        }
+    y = 160;
+    while (y < 725)
+        {
+        y = y + 15;
+        txSetFillColor(TX_WHITE);
+        txClear();
+        tree_draw();
+        bird_draw(1125, 225, 0.4, 0,0,-1);
 
-*/
+        fox_draw (1275, 650, 0.8, 0, 0, 0, 0, 0, -1);
+
+        cheese(1275, y, 1);
+        txSleep(50);
+
+
+        }
+        x = 1275;
+    while (x > -100)
+        {
+        x = x - 30;
+        txSetFillColor(TX_WHITE);
+        txClear();
+        tree_draw();
+        bird_draw(1125, 225, 0.4, 0,0,-1);
+        fox_draw (x, 650, 0.8, 0, 0, 0, 0, 0, -1);
+        cheese(x - 50, 725, 1);
+        txSleep(50);
+
+
+
+        }
+        }
+
+/*
 while (y < 775)
     {
     x = 300;
     y = y + 10;
     txSetFillColor(TX_WHITE);
     txClear();
+    tree_draw();
     bird_draw (x, y, 0.5, 0, 0, -1);
-    txSleep(150);
+    txSleep(50);
     }
 
-/*
+
     while ( y < 775)
      {
         x = 300;
@@ -71,27 +153,31 @@ while (y < 775)
 
 
      }
-*/
+
+   double size = 0.5;
    while (x < 1100)
         {
+
         if (z % 4 == 4)   {x = x + 40;}
         else              {x = x + 15; }
         z++;
         y = y - 10;
+        size = size - 0.002;
+
+
         txSetFillColor(TX_WHITE);
         txClear();
-
-        bird_draw (x, y, 0.5, 0, 0, -1);
-        cheese(x,y,1);
+        tree_draw();
+        bird_draw (x, y, size, -10, 10, -1);
+        cheese(x + 150,y - 70, 1);
         txSleep(150);
-
         }
-       tree_draw();
     }
+*/
 
 //-----------------------------------------------------------------------------
 void bird_draw (double x, double y, double size, double dLegL, double dLegR, int mirror)
-{
+    {
     txSetFillColor(TX_TRANSPARENT);
     txSetColor(TX_BLACK, 7*size);
 
@@ -170,14 +256,14 @@ void fox_draw (double x, double y, double size, double dHandL, double dHandR, do
 
     //hand
     txSetFillColor(TX_ORANGE);
-    POINT HandL[3] = {{ROUND(x +  10*mirror), ROUND(y +  75       )},
-                      {ROUND(x + 150*mirror), ROUND(y +  75+dHandL)},
-                      {ROUND(x +  25*mirror), ROUND(y + 115       )}};
+    POINT HandL[3] = {{ROUND(x +  10*size*mirror), ROUND(y +  75*size       )},
+                      {ROUND(x + 150*size*mirror), ROUND(y +  75*size+dHandL)},
+                      {ROUND(x +  25*size*mirror), ROUND(y + 115*size       )}};
     txPolygon(HandL, 3);
 
-    POINT HandR[3] = {{ROUND(x +  30*mirror), ROUND(y +  125       )},
-                      {ROUND(x + 150*mirror), ROUND(y +  150+dHandR)},
-                      {ROUND(x +  35*mirror), ROUND(y +  150       )}};
+    POINT HandR[3] = {{ROUND(x +  30*size*mirror), ROUND(y +  125*size       )},
+                      {ROUND(x + 150*size*mirror), ROUND(y +  150*size+dHandR)},
+                      {ROUND(x +  35*size*mirror), ROUND(y +  150*size       )}};
     txPolygon(HandR, 3);
 
     //tail
@@ -190,12 +276,12 @@ void fox_draw (double x, double y, double size, double dHandL, double dHandR, do
     //head
     POINT head[5] =  {{ROUND(x -  50*size*mirror), ROUND(y - 100*size)},
                       {ROUND(x),                   ROUND(y - 100*size)},
-                      {ROUND(x + 150*size*mirror), ROUND(y - 50 + dNose*size)},
+                      {ROUND(x + 150*size*mirror), ROUND(y - 50*size + dNose)},
                       {ROUND(x),                   ROUND(y)},
                       {ROUND(x -  50*size*mirror), ROUND(y)}};
     txPolygon(head, 5);
     txSetColor(TX_BLACK, 10*size);
-    txCircle(x + 150*size*mirror, y - 50 + dNose*size, 4*size);
+    txCircle(x + 150*size*mirror, y - 50*size + dNose, 4*size);
 
     //Eye
     txCircle(x, y - 75*size, 5*size);
@@ -222,7 +308,6 @@ void tree (double x, double y, double size)
                         {ROUND (x + 125*size),ROUND    (y + 150*size)}};
     txPolygon(tree,3);
     }
-
 void tree_draw ()
     {
     txSetColor(TX_BLACK, 7);
@@ -231,24 +316,18 @@ void tree_draw ()
     tree (1100, 500, 1);
     txSetFillColor(TX_BROWN);
     txRectangle(1075, 650, 1125,  750);
-
-
-
     }
 
 void cheese (double x, double y, double size)
         {
+        txSetColor(TX_TRANSPARENT);
         POINT Cheese[3] =    {{ROUND (x      *size),ROUND      (y      *size)},
                               {ROUND (x + 35 *size),ROUND      (y + 30*size) },
                               {ROUND (x - 65 *size),ROUND      (y + 30*size)}};
         txSetFillColor(RGB(255, 235, 54));
         txPolygon(Cheese, 3);
-
-
-
-
-
-
-
         }
+
+
+
 
